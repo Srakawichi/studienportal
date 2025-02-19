d953d9aefc0dd5b6000c0a42f7b4f3ab68099d75
snaan5 <siyur2005@gmail.com>
2025-02-19 13:38:15 +0100

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import ImageSlider from "./dashboard/slideshow";
 import Navbar from "./navigation/navbar";
 
 export default function Home() {
   return (
       <div>
           <Navbar/>
-          <ImageSlider />
           <div className="flex min-h-screen place-content-end">
               <div className="bg-gray-200 p-6 rounded-lg shadow-md w-[400px] h-[600px] mr-32 mt-16">
                   <p className="text-lg font-medium text-center">Neuigkeiten & Aktivitäten</p>
